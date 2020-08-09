a569b3627e2cfbc5c20ce0fa4504dc381b33a523
Adi52 <adimr52@gmail.com>
2020-08-09 02:39:39 +0200

    Second commit

--- main.js
+++ main.js
@@ -21,29 +21,34 @@ burgerBtn.addEventListener('click', function () {
 (function() {
     let button = document.getElementById('button_doom_id');
     button.addEventListener('click', function () {
-        let image = document.getElementById('image_doom_id');
-        if (image.style.display === 'block') {
-            image.style.display = 'none';
-            button.innerText = 'Pokaż obrazek';
-        } else {
-            image.style.display = 'block';
+        let imageDiv = document.querySelector('.screen');
+        if (imageDiv.offsetHeight === 0){
+            let image = document.getElementById('image_doom_id');
+            imageDiv.style.height = `${image.offsetHeight}px`;
             button.innerText = 'Schowaj obrazek';
+        } else {
+            imageDiv.style.height = '0px';
+            button.innerText = 'Pokaż obrazek';
         }
     });
 }());
 
+
+
+
 const dlcWrapper = document.querySelector('.content-quiz .dlc-wrapper');
 const dlcExpand = document.querySelector('.content-quiz .expand ');
+const dlcBtn = document.querySelector('.container-quiz button.dlc');
 
-document.querySelector('button.dlc').addEventListener('click', function() {
+dlcBtn.addEventListener('click', function() {
     document.querySelector('button.dlc').disabled = 'true'
-   dlcWrapper.style.display = 'flex';
-   dlcExpand.style.display = 'block';
-   document.querySelector('.answer .correct').checked = 'true';
-   document.querySelector('.quiz .answer i').style.display = 'inline-block';
-   document.querySelector('.container-quiz button.dlc').style.backgroundColor = '#d07501';
-   document.querySelector('.container-quiz button.dlc').classList.remove('shake');
-   angryDevil();
+    let heightDiv = dlcWrapper.offsetHeight + dlcExpand.offsetHeight;
+    document.querySelector('.content-quiz .dlc-full').style.height = `${heightDiv}px`;
+    document.querySelector('.answer .correct').checked = 'true';
+    document.querySelector('.quiz .answer i').style.display = 'inline-block';
+    dlcBtn.style.opacity = '0';
+    angryDevil();
+    setTimeout(function() {document.querySelector('.content-quiz .dlc-full').style.height = 'auto'}, 300);
 });
 
 function angryDevil() {
@@ -51,9 +56,11 @@ function angryDevil() {
     setTimeout(function() {
         document.querySelector('.devil span.devil-talk').style.display = 'none';
         devilImg.src = 'img/devil/devil2.png';
+        devilImg.classList.add('active');
         document.querySelector('.mo-fire').classList.add('active');
+        dlcBtn.style.display = 'none';
     }, 2000);
-    setTimeout(function() {devilImg.src = 'img/devil/devil3.png'}, 2050);
+
 
 
 
